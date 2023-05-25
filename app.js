803a0c595f04c4352c6e702cd162861448051e24
mohammadxxali <ali_mohammad@t-online.de>
2023-05-25 10:37:14 +0200

    add js app edits to getTime

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ function calculate() {
     setInterval(() => {
         // Input Birthdate //
         let birthdate = new Date(document.getElementById('birthdate').value);
+
+        // getTime : new Date //
+        let now = new Date();
+        let ageInMs = now.getTime() - birthdate.getTime();
+
+        let ageInS = ageInMs / 1000; // to seconds //
+        let ageInMins = ageInS / 60; // to minutes //
+        let ageInHrs = ageInMins / 60; // to Hours //
+        let ageInDays = ageInHrs / 24; // to days //
+        let ageInMonths = ageInDays / 30.4375; // to months //
+        let ageInYears = ageInMonths / 12; // to years //
+        
     })
 }
